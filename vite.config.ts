752e09642906d4c5e84eb935f8f3be924a423868
Eduardo Escobar <edu.escobarp15@gmail.com>
2026-02-05 00:33:24 -0300

    Tarea: Actualizar package.json y vite.config.ts para la configuración de la implementación.
    - Se añadió la dependencia "gh-pages" a package.json para la implementación de GitHub Pages.
    - Se actualizó el campo de página de inicio en package.json para la URL del proyecto.
    - Se añadieron scripts de preimplementación e implementación en package.json para compilar e implementar el proyecto.
    - Se aseguró que la ruta base esté configurada correctamente en vite.config.ts para GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/glowupdigitaldesing",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
